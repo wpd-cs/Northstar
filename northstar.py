10cbcdf35b3799214b098613342a0825760c167e
William Duong <altwpd@gmail.com>
2021-11-19 11:48:33 -0800

    Added a lot of functionality

--- northstar.py
+++ northstar.py
@@ -9,34 +9,57 @@ Last Updated: 11/17/2021
 
 """
 
+from sys import exit
+from multiprocessing import Process
+import datetime
+import csv
+import os
 
-class Populations(self):
-	self.cStudents = []
-	self.fStudents = []
-	self.faculty = []
-	self.staff = []
-	self.asc = []
-	self.asi = []
-	self.nonstateStaff
-	self.unspecified = []
+class Populations:
+	def __init__(self):
+		self.cStudents = []
+		self.fStudents = []
+		self.faculty = []
+		self.staff = []
+		self.asc = []
+		self.asi = []
+		self.nonstateStaff = []
+		self.unspecified = []
 
-	self.compliant = []
-	self.aReview = []
-	self.cExemption = []
-	self.eExemption = []
-	self.mExemption = []
-	self.rExemption = []
-	self.pExemption = []
-	self.bExemption = []
-	self.notCompliant = []
+		self.compliant = []
+		self.aReview = []
+		self.cExemption = []
+		self.eExemption = []
+		self.mExemption = []
+		self.rExemption = []
+		self.pExemption = []
+		self.bExemption = []
+		self.notCompliant = []
+
+	def getAllTypes(self):
+		return self.cStudents + \
+			   self.fStudents + \
+			   self.faculty + \
+			   self.staff + \
+			   self.asc + \
+			   self.asi + \
+			   self.nonstateStaff + \
+			   self.unspecified
 
 	def getCompliant(self):
 		"""Return all compliant patients"""
 		return self.compliant
 
+	def getNotCompliant(self):
+		"""Return all non-compliant patients"""
+		return self.notCompliant
+
+	def getExtensionExemptions(self):
+		"""Return all extension exemptions"""
+		return self.eExemption
+
 	def getExemptions(self):
-		return 	self.eExemption + \
-				self.mExemption + \
+		return 	self.mExemption + \
 				self.rExemption + \
 				self.pExemption + \
 				self.bExemption
@@ -54,8 +77,18 @@ class Populations(self):
 			   self.bExemption
 
 	def getActiveNotCompliant(self):
-		"""copy this: https://blog.finxter.com/how-to-get-specific-elements-from-a-list/#Get_Elements_from_List_using_filter"""
-		pass
+		"""Return all non-compliant active patients"""
+		def dist(patient):
+			return patient.getPatientType()
+
+		def intersection(l1, l2):
+			active = set(l2)
+			l3 = [v for v in l1 if value in active]
+			return l3
+
+		active = list(filter(lambda x: dist(x) == "", self.getAllTypes()))
+
+		return intersection(active, self.getNotCompliant())
 
 
 
@@ -83,7 +116,6 @@ class Patient:
 		"""Get academic status"""
 		return self.__acadStatus
 
-
 	def setPatientType(self, patientType):
 		"""Set patient type"""
 		self.__patientType = patientType
@@ -98,8 +130,25 @@ class Patient:
 
 
 
+def checkFiles():
+	"""Check to make sure all input files are present"""
+	listOfFiles = ["compliance.txt", "employee.txt", "student.txt", \
+				   "nonstate.txt"]
+
+	filesNeeded = []
+
+	for file in listOfFiles:
+		if not os.path.exists(file):
+			filesNeeded.append(file)
+
+	if filesNeeded:
+		exit("ERROR\nMissing files: {}".format(filesNeeded))
+
+
+
 def readInEmployees(populations):
 	"""Read in employee extract"""
+	print("1 starting\n")
 	with open("employee.txt", "r") as f:
 		temp = f.readline()
 		del temp
@@ -113,11 +162,13 @@ def readInEmployees(populations):
 				populations.faculty.append(patient)
 			else:
 				populations.staff.append(patient)
+	print("1 finishing\n")
 
 
 
 def readInStudents(populations):
 	"""Read in student extract"""
+	print("2 starting\n")
 	with open("student.txt", "r") as f:
 		temp = f.readline()
 		del temp
@@ -131,11 +182,13 @@ def readInStudents(populations):
 				populations.cStudents.append(patient)
 			else:
 				populations.fStudents.append(patient)
+	print("2 finishing\n")
 
 
 
 def readInNonState(populations):
 	"""Read in non-state extract"""
+	print("3 starting\n")
 	with open("nonstate.txt", "r") as f:
 		temp = f.readline()
 		del temp
@@ -147,15 +200,17 @@ def readInNonState(populations):
 
 			if patient.getPatientType() == "ASC":
 				populations.asc.append(patient)
-			else if patient.getPatientType() == "ASI":
+			elif patient.getPatientType() == "ASI":
 				populations.asi.append(patient)
 			else:
 				populations.nonstateStaff.append(patient)
+	print("3 finishing\n")
 
 
 
 def readInCompliance(populations):
 	"""Read in PNC Data"""
+	print("4 starting\n")
 	with open("compliance.txt", "r") as f:
 		temp = f.readline()
 		del temp
@@ -168,37 +223,63 @@ def readInCompliance(populations):
 
 			patient = Patient(cwid, status = pstatus)
 
-			if patient.getStatus() == '"Compliant with Standard Requirements"':
-				populations.compliant.append(patient)
-			if patient.getStatus() == '"Awaiting Review"':
-				populations.aReview.append(patient)
-			if patient.getStatus() == '"Exemption: Pos COVID-19 90 Days"':
-				populations.cExemption.append(patient)
-			if patient.getStatus() == '"Exemption: Extension COVID-19"':
-				populations.eExemption.append(patient)
-			if patient.getStatus() == '"Exemption: Medical COVID-19"':
-				populations.mExemption.append(patient)
-			if patient.getStatus() == '"Exemption: Religious COVID-19"':
-				populations.rExemption.append(patient)
-			if patient.getStatus() == '"Exemption: Pregnant COVID-19"':
-				populations.pExemption.append(patient)
-			if patient.getStatus() == '"Exemption: Breast Feeding COVID"':
-				populations.bExemption.append(patient)
-			else:
-				populations.notCompliant.append(patient)
+			match patient.getStatus():
+				case '"Compliant with Standard Requirements"':
+					populations.compliant.append(patient)
+				case '"Awaiting Review"':
+					populations.aReview.append(patient)
+				case '"Exemption: Pos COVID-19 90 Days"':
+					populations.cExemption.append(patient)
+				case '"Exemption: Extension COVID-19"':
+					populations.eExemption.append(patient)
+				case '"Exemption: Medical COVID-19"':
+					populations.mExemption.append(patient)
+				case '"Exemption: Religious COVID-19"':
+					populations.rExemption.append(patient)
+				case '"Exemption: Pregnant COVID-19"':
+					populations.pExemption.append(patient)
+				case '"Exemption: Breast Feeding COVID"':
+					populations.bExemption.append(patient)
+				case _:
+					populations.notCompliant.append(patient)
+	print("4 finishing\n")
 
 
 
+def concurrent(*functions):
+	proc = []
+	for fn in functions:
+		p = Process(target=fn)
+		p.start()
+		proc.append(p)
+	for p in proc:
+		p.join()
 
 
 
-def main():
+populations = Populations()
+
+
+
+if __name__ == "__main__":
 	"""Main function"""
 
 	# Initialize variables
 
-	populations = Populations()
+	# populations = Populations()
+
+	concurrent(readInEmployees(populations), readInStudents(populations), \
+			   readInNonState(populations), readInCompliance(populations))
+	
+	p = populations.getActiveNotCompliant()
+
+	a = [[item.getCwid()] for item in p]
+
+	f = open("test.csv", "w", newline='')
+
+	with f:
+		write = csv.writer(f)
+		write.writerows(a)
 
 
-
-main()
+	f.close()
