ff073ceb0d0767d83fd46ef58f33ba4af5d83e60
William Duong <altwpd@gmail.com>
2022-02-02 08:54:51 -0800

    Added script file

---
+++ runScript.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+::This batch file helps streamline the process of running the compliancy counter.
+TITLE Running the Compliancy Counter...
+ECHO Starting processes...
+ECHO.
+"C:\Users\zz-wduong\AppData\Local\Programs\Python\Python310\python.exe" ^
+"C:\Users\zz-wduong\Dropbox (CSU Fullerton)\PC\Desktop\Northstar\northstar.py"
+PAUSE
